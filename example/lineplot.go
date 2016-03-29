50ee731332b5588d5ad9a47c15296af2316bc6a6
Jason DeVita <jason@wx13.com>
2016-03-29 16:15:38 -0700

    humble begginings of dygraphs for go
    	new file:   dygraphs.go
    	new file:   example/lineplot.go
    	new file:   graph.go

---
+++ example/lineplot.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wx13/dygoraphs"
+)
+
+func main() {
+	fmt.Println(dygoraphs.Script)
+}
+
---
+++ graph.go
@@ -0,0 +1,5 @@
+package dygoraphs
+
+type Graph struct {
+}
+
