d6a991b6e30c6cce7a32dd401fead125844d5d93
Jason DeVita <jason@wx13.com>
2016-03-29 17:01:40 -0700

    basic lineplot is working!
    	modified:   example/lineplot.go
    	modified:   graph.go

--- example/lineplot.go
+++ example/lineplot.go
@@ -6,6 +6,21 @@ import (
 )
 
 func main() {
+	fmt.Println("<html><head><title></title>")
 	fmt.Println(dygoraphs.Script)
+	fmt.Println("</head><body>")
+	fmt.Println("<h1>Line Plot</h1>")
+
+	g := dygoraphs.NewGraph()
+	cols := []string{"time", "height", "weight"}
+	data := [][]float64{
+		[]float64{0, 1, 2, 3, 4, 5},
+		[]float64{100, 102, 108, 109, 110, 110},
+		[]float64{151, 149, 147, 150, 155, 160},
+	}
+	g.Add(cols, data)
+	fmt.Println(g.Plot())
+
+	fmt.Println("</body></html>")
 }
 
--- graph.go
+++ graph.go
@@ -1,5 +1,60 @@
 package dygoraphs
 
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
 type Graph struct {
+	Id      string
+	Columns []string
+	Data    [][]float64
 }
 
+func NewGraph() *Graph {
+	g := Graph{}
+	g.Id = fmt.Sprintf("%d", rand.Int())
+	g.Columns = []string{}
+	g.Data = [][]float64{}
+	return &g
+}
+
+func (g *Graph) Add(columns []string, data [][]float64) {
+	g.Columns = append(g.Columns, columns...)
+	g.Data = append(g.Data, data...)
+}
+
+func (g *Graph) CSVdata() string {
+	csv := `"` + strings.Join(g.Columns, ",") + `\n"` + " +\n"
+	for row := range g.Data[0] {
+		csv += `"`
+		for col := range g.Data {
+			csv += fmt.Sprintf("%f", g.Data[col][row])
+			if col < len(g.Data)-1 {
+				csv += ","
+			}
+		}
+		csv += `\n"`
+		if row < len(g.Data[0])-1 {
+			csv += " +\n"
+		}
+	}
+	return csv
+}
+
+func (g *Graph) Div() string {
+	return fmt.Sprintf(`<div id="%s"></div>`, g.Id)
+}
+
+func (g *Graph) Plot() string {
+	csv := g.CSVdata()
+	html := g.Div() + "\n"
+	html += "<script>\n"
+	html += "g = new Dygraph(\n"
+	html += fmt.Sprintf(`  document.getElementById("%s"),`, g.Id) + "\n"
+	html += csv
+	html += ");\n"
+	html += "</script>\n"
+	return html
+}
