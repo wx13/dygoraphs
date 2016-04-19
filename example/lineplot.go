a9577751053201c58ee7d7a0f125e63eb347ec6f
Jason DeVita <jdevita@arete.com>
2016-04-19 12:02:27 -0700

    complete rewrite -- now just plotting CSV data
    	modified:   example/lineplot.go
    	modified:   graph.go

--- example/lineplot.go
+++ example/lineplot.go
@@ -10,35 +10,76 @@ func LinePlot() {
 	fmt.Println("<h2>Line Plot</h2>")
 
 	g := dygoraphs.NewGraph()
-	g.Style = "width:500px; height:300px;"
-	cols := []string{"time", "height", "weight"}
-	data := [][]float64{
-		[]float64{0, 1, 2, 3, 4, 5},
-		[]float64{100, 102, 108, 109, 110, 110},
-		[]float64{151, 149, 147, 150, 155, 160},
-	}
-	g.Add(cols, data)
-	fmt.Println(g.Plot())
+	g.SetStyle("width", "500px")
+	g.SetStyle("height", "300px")
+	data := "time,height,weight\n" +
+		"0,100,150\n" +
+		"1,102,140\n" +
+		"2,104,145\n" +
+		"3,101,155\n" +
+		"4,110,160\n" +
+		"5,105,170\n"
+	fmt.Println(g.PlotCSV(data))
 
 }
 
-func ErrorBars() {
+func SymErrBars() {
 
-	fmt.Println("<h2>Error Bars</h2>")
+	fmt.Println("<h2>Symmetric Error Bars</h2>")
 
 	g := dygoraphs.NewGraph()
-	g.Style = "width:500px; height:300px;"
-	cols := []string{"time", "height", "weight"}
-	data := [][]float64{
-		[]float64{0, 1, 2, 3, 4, 5},
-		[]float64{100, 102, 108, 109, 110, 110},
-		[]float64{10, 10, 8, 9, 3, 10},
-		[]float64{151, 149, 147, 150, 155, 160},
-		[]float64{20, 25, 15, 10, 25, 20},
-	}
-	g.Options = "{errorBars: true}"
-	g.Add(cols, data)
-	fmt.Println(g.Plot())
+	g.SetStyle("width", "500px")
+	g.SetStyle("height", "300px")
+	g.SetOption("errorBars", "true")
+	data := "time,height,weight\n" +
+		"0,100,10,150,20\n" +
+		"1,102,10,140,20\n" +
+		"2,104,10,145,20\n" +
+		"3,101,10,155,20\n" +
+		"4,110,10,160,20\n" +
+		"5,105,10,170,20\n"
+	fmt.Println(g.PlotCSV(data))
+
+}
+
+func AsymErrBars() {
+
+	fmt.Println("<h2>Asymmetric Error Bars</h2>")
+
+	g := dygoraphs.NewGraph()
+	g.SetStyle("width", "500px")
+	g.SetStyle("height", "300px")
+	g.SetOption("customBars", "true")
+	data := "time,height,weight\n" +
+		"0,95;100;110,140;150;160\n" +
+		"1,100;102;112,140;140;160\n" +
+		"2,100;104;114,140;145;160\n" +
+		"3,100;101;111,140;155;160\n" +
+		"4,100;110;129,140;160;160\n" +
+		"5,100;105;115,140;170;160\n"
+	fmt.Println(g.PlotCSV(data))
+
+}
+
+func ErrBarTrans() {
+
+	fmt.Println("<h2>Symmetric Error Bars</h2>")
+
+	g := dygoraphs.NewGraph()
+	g.SetStyle("width", "500px")
+	g.SetStyle("height", "300px")
+	g.SetOption("errorBars", "true")
+	g.SetOption(`'height'`, "{color: \"blue\", fillAlpha: 0.5}")
+	g.SetOption(`'weight'`, "{color: \"blue\", fillAlpha: 0.1}")
+	g.SetOption(`'depth'`, "{color: \"blue\", fillAlpha: 0.3}")
+	data := "time,height,weight,depth\n" +
+		"0,100,10,150,20,80,10\n" +
+		"1,102,10,140,20,80,10\n" +
+		"2,104,10,145,20,80,10\n" +
+		"3,101,10,155,20,80,10\n" +
+		"4,110,10,160,20,80,10\n" +
+		"5,105,10,170,20,80,10\n"
+	fmt.Println(g.PlotCSV(data))
 
 }
 
@@ -49,7 +90,9 @@ func main() {
 	fmt.Println("</head><body>")
 
 	LinePlot()
-	ErrorBars()
+	SymErrBars()
+	AsymErrBars()
+	ErrBarTrans()
 
 	fmt.Println("</body></html>")
 
--- graph.go
+++ graph.go
@@ -8,57 +8,77 @@ import (
 
 type Graph struct {
 	Id      string
-	Columns []string
-	Data    [][]float64
-	Style   string
-	Options string
+	Style   map[string]string
+	Options map[string]string
 }
 
 func NewGraph() *Graph {
 	g := Graph{}
 	g.Id = fmt.Sprintf("%d", rand.Int())
-	g.Columns = []string{}
-	g.Data = [][]float64{}
-	g.Style = ""
-	g.Options = "{}"
+	g.Style = make(map[string]string)
+	g.Options = make(map[string]string)
 	return &g
 }
 
-func (g *Graph) Add(columns []string, data [][]float64) {
-	g.Columns = append(g.Columns, columns...)
-	g.Data = append(g.Data, data...)
+func (g *Graph) SetOption(name, value string) {
+	g.Options[name] = value
 }
 
-func (g *Graph) CSVdata() string {
-	csv := `"` + strings.Join(g.Columns, ",") + `\n"` + " +\n"
-	for row := range g.Data[0] {
-		csv += `"`
-		for col := range g.Data {
-			csv += fmt.Sprintf("%f", g.Data[col][row])
-			if col < len(g.Data)-1 {
-				csv += ","
-			}
-		}
-		csv += `\n"`
-		if row < len(g.Data[0])-1 {
-			csv += " +\n"
-		}
-	}
-	return csv
+func (g *Graph) SetStyle(name, value string) {
+	g.Style[name] = value
 }
+
+func (g *Graph) DumpOptions() string {
+	optStrs := []string{}
+	for name, val := range g.Options {
+		s := fmt.Sprintf("%s: %s", name, val)
+		optStrs = append(optStrs, s)
+	}
+	return "{" + strings.Join(optStrs, ", ") + "}"
+}
+
+func (g *Graph) DumpStyle() string {
+	strs := []string{}
+	for name, val := range g.Style {
+		s := fmt.Sprintf("%s:%s", name, val)
+		strs = append(strs, s)
+	}
+	return strings.Join(strs, "; ")
+}
+
+//func (g *Graph) CSVdata() string {
+//	csv := `"` + strings.Join(g.Columns, ",") + `\n"` + " +\n"
+//	for row := range g.Data[0] {
+//		csv += `"`
+//		for col := range g.Data {
+//			csv += fmt.Sprintf("%f", g.Data[col][row])
+//			if col < len(g.Data)-1 {
+//				csv += ","
+//			}
+//		}
+//		csv += `\n"`
+//		if row < len(g.Data[0])-1 {
+//			csv += " +\n"
+//		}
+//	}
+//	return csv
+//}
 
 func (g *Graph) Div() string {
-	return fmt.Sprintf(`<div id="%s" style="%s"></div>`, g.Id, g.Style)
+	return fmt.Sprintf(`<div id="%s" style="%s"></div>`, g.Id, g.DumpStyle())
 }
 
-func (g *Graph) Plot() string {
-	csv := g.CSVdata()
+func (g *Graph) EscCSV(csv string) string {
+	return fmt.Sprintf("%q", csv)
+}
+
+func (g *Graph) PlotCSV(csv string) string {
 	html := g.Div() + "\n"
 	html += "<script>\n"
 	html += "g = new Dygraph(\n"
 	html += fmt.Sprintf(`  document.getElementById("%s"),`, g.Id) + "\n"
-	html += csv + ",\n"
-	html += g.Options
+	html += g.EscCSV(csv) + ",\n"
+	html += g.DumpOptions()
 	html += ");\n"
 	html += "</script>\n"
 	return html
